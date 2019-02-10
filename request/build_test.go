98473ee378608618a2de75648e278e897ef1b1ec
Yusuke Nojima <nojima@ynojima.com>
2019-02-10 21:58:50 +0900

    Write a test for buildHttpBody()

--- request/build_test.go
+++ request/build_test.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -50,5 +52,123 @@ func TestBuildHttpHeader(t *testing.T) {
 	}
 	if !reflect.DeepEqual(httpHeader, expected) {
 		t.Errorf("unexpected header: expected=%v, actual=%v", expected, httpHeader)
+	}
+}
+
+func isEquivalentJson(t *testing.T, json1, json2 string) bool {
+	var obj1, obj2 interface{}
+	if err := json.Unmarshal([]byte(json1), &obj1); err != nil {
+		t.Errorf("failed to unmarshal json1: %v", err)
+	}
+	if err := json.Unmarshal([]byte(json2), &obj2); err != nil {
+		t.Errorf("failed to unmarshal json2: %v", err)
+	}
+	return reflect.DeepEqual(obj1, obj2)
+}
+
+func readAll(t *testing.T, reader io.Reader) string {
+	b, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Errorf("failed to read all: %s", err)
+	}
+	return string(b)
+}
+
+func TestBuildHttpBody_EmptyBody(t *testing.T) {
+	// Setup
+	fileName := makeTempFile(t, "test test")
+	defer os.Remove(fileName)
+	body := input.Body{
+		BodyType: input.EmptyBody,
+	}
+	request := &input.Request{Body: body}
+
+	// Exercise
+	actual, err := buildHttpBody(request)
+	if err != nil {
+		t.Errorf("unexpected error: err=%+v", err)
+	}
+
+	// Verify
+	expected := bodyTuple{}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("unexpected body tuple: expected=%+v, actual=%+v", expected, actual)
+	}
+}
+
+func TestBuildHttpBody_JsonBody(t *testing.T) {
+	// Setup
+	fileName := makeTempFile(t, "test test")
+	defer os.Remove(fileName)
+	body := input.Body{
+		BodyType: input.JsonBody,
+		Fields: []input.Field{
+			{Name: "foo", Value: "bar"},
+			{Name: "from_file", Value: fileName, IsFile: true},
+		},
+		RawJsonFields: []input.Field{
+			{Name: "boolean", Value: "true"},
+			{Name: "array", Value: `[1, null, "hello"]`},
+		},
+	}
+	request := &input.Request{Body: body}
+
+	// Exercise
+	bodyTuple, err := buildHttpBody(request)
+	if err != nil {
+		t.Errorf("unexpected error: err=%+v", err)
+	}
+
+	// Verify
+	expectedBody := `{
+		"foo": "bar",
+		"from_file": "test test",
+		"boolean": true,
+		"array": [1, null, "hello"]
+	}`
+	actualBody := readAll(t, bodyTuple.body)
+	if !isEquivalentJson(t, expectedBody, actualBody) {
+		t.Errorf("unexpected body: expected=%s, actual=%s", expectedBody, actualBody)
+	}
+	expectedContentType := "application/json"
+	if bodyTuple.contentType != expectedContentType {
+		t.Errorf("unexpected content type: expected=%s, actual=%s", expectedContentType, bodyTuple.contentType)
+	}
+	if bodyTuple.contentLength != int64(len(actualBody)) {
+		t.Errorf("invalid content length: len(body)=%v, actual=%v", len(actualBody), bodyTuple.contentLength)
+	}
+}
+
+func TestBuildHttpBody_FormBody(t *testing.T) {
+	// Setup
+	fileName := makeTempFile(t, "love & peace")
+	defer os.Remove(fileName)
+	body := input.Body{
+		BodyType: input.FormBody,
+		Fields: []input.Field{
+			{Name: "foo", Value: "bar"},
+			{Name: "from_file", Value: fileName, IsFile: true},
+		},
+	}
+	request := &input.Request{Body: body}
+
+	// Exercise
+	bodyTuple, err := buildHttpBody(request)
+	if err != nil {
+		t.Errorf("unexpected error: err=%+v", err)
+	}
+
+	// Verify
+	expectedBody := `foo=bar&from_file=love+%26+peace`
+	actualBody := readAll(t, bodyTuple.body)
+	if actualBody != expectedBody {
+		t.Errorf("unexpected body: expected=%s, actual=%s", expectedBody, actualBody)
+	}
+	expectedContentType := "application/x-www-form-urlencoded; charset=utf-8"
+	if bodyTuple.contentType != expectedContentType {
+		t.Errorf("unexpected content type: expected=%s, actual=%s", expectedContentType, bodyTuple.contentType)
+	}
+	if bodyTuple.contentLength != int64(len(actualBody)) {
+		t.Errorf("invalid content length: len(body)=%v, actual=%v", len(actualBody), bodyTuple.contentLength)
 	}
 }
