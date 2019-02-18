bec776670aeecb898b080d00109a387205e7b9fa
Yusuke Nojima <nojima@ynojima.com>
2019-02-19 00:48:46 +0900

    refactor: extract flag parser to dedicated package

---
+++ flags/flags.go
@@ -0,0 +1,92 @@
+package flags
+
+import (
+	"os"
+	"regexp"
+	"time"
+
+	"github.com/mattn/go-isatty"
+	"github.com/nojima/httpie-go/input"
+	"github.com/nojima/httpie-go/output"
+	"github.com/nojima/httpie-go/request"
+	"github.com/pborman/getopt"
+	"github.com/pkg/errors"
+)
+
+var reNumber = regexp.MustCompile(`^[0-9.]+$`)
+
+func Parse(args []string) (*getopt.Set, *input.Options, *request.Options, *output.Options, error) {
+	// Parse flags
+	inputOptions := &input.Options{}
+	outputOptions := &output.Options{}
+	requestOptions := &request.Options{}
+	var ignoreStdin bool
+	printFlag := "\000" // "\000" is a special value that indicates user did not specified --print
+	timeout := "30s"
+
+	flagSet := getopt.New()
+	flagSet.SetParameters("[METHOD] URL [REQUEST_ITEM [REQUEST_ITEM ...]]")
+	flagSet.BoolVarLong(&inputOptions.Form, "form", 'f', "serialize body in application/x-www-form-urlencoded")
+	flagSet.StringVarLong(&printFlag, "print", 'p', "specifies what the output should contain (HBhb)")
+	flagSet.BoolVarLong(&ignoreStdin, "ignore-stdin", 0, "do not attempt to read stdin")
+	flagSet.StringVarLong(&timeout, "timeout", 0, "Timeout seconds that you allow the whole operation to take")
+	flagSet.Parse(args)
+
+	// Check stdin
+	if !ignoreStdin && !isatty.IsTerminal(os.Stdin.Fd()) {
+		inputOptions.ReadStdin = true
+	}
+
+	// Parse --print
+	if err := parsePrintFlag(printFlag, outputOptions); err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	// Parse --timeout
+	d, err := parseDurationOrSeconds(timeout)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	requestOptions.Timeout = d
+
+	return flagSet, inputOptions, requestOptions, outputOptions, nil
+}
+
+func parsePrintFlag(printFlag string, outputOptions *output.Options) error {
+	if printFlag == "\000" {
+		// --print is not specified
+		if isatty.IsTerminal(os.Stdout.Fd()) {
+			outputOptions.PrintResponseHeader = true
+			outputOptions.PrintResponseBody = true
+		} else {
+			outputOptions.PrintResponseBody = true
+		}
+	} else {
+		for _, c := range printFlag {
+			switch c {
+			case 'H':
+				outputOptions.PrintRequestHeader = true
+			case 'B':
+				outputOptions.PrintRequestBody = true
+			case 'h':
+				outputOptions.PrintResponseHeader = true
+			case 'b':
+				outputOptions.PrintResponseBody = true
+			default:
+				return errors.Errorf("Invalid char in --print value (must be consist of HBhb): %c", c)
+			}
+		}
+	}
+	return nil
+}
+
+func parseDurationOrSeconds(timeout string) (time.Duration, error) {
+	if reNumber.MatchString(timeout) {
+		timeout += "s"
+	}
+	d, err := time.ParseDuration(timeout)
+	if err != nil {
+		return time.Duration(0), errors.Errorf("Value of --timeout must be a number or duration string: %v", timeout)
+	}
+	return d, nil
+}
--- main.go
+++ main.go
@@ -3,21 +3,18 @@ package httpie
 import (
 	"bufio"
 	"os"
-	"regexp"
-	"time"
 
 	"github.com/mattn/go-isatty"
+	"github.com/nojima/httpie-go/flags"
 	"github.com/nojima/httpie-go/input"
 	"github.com/nojima/httpie-go/output"
 	"github.com/nojima/httpie-go/request"
-	"github.com/pborman/getopt"
 	"github.com/pkg/errors"
 )
 
-var reNumber = regexp.MustCompile(`^[0-9.]+$`)
-
 func Main() error {
-	flagSet, inputOptions, requestOptions, outputOptions, err := parseFlags()
+	// Parse flags
+	flagSet, inputOptions, requestOptions, outputOptions, err := flags.Parse(os.Args)
 	if err != nil {
 		return err
 	}
@@ -64,78 +61,3 @@ func Main() error {
 	return nil
 }
 
-func parseFlags() (*getopt.Set, *input.Options, *request.Options, *output.Options, error) {
-	// Parse flags
-	inputOptions := &input.Options{}
-	outputOptions := &output.Options{}
-	requestOptions := &request.Options{}
-	var ignoreStdin bool
-	printFlag := "\000" // "\000" is a special value that indicates user did not specified --print
-	timeout := "30s"
-
-	flagSet := getopt.New()
-	flagSet.SetParameters("[METHOD] URL [REQUEST_ITEM [REQUEST_ITEM ...]]")
-	flagSet.BoolVarLong(&inputOptions.Form, "form", 'f', "serialize body in application/x-www-form-urlencoded")
-	flagSet.StringVarLong(&printFlag, "print", 'p', "specifies what the output should contain (HBhb)")
-	flagSet.BoolVarLong(&ignoreStdin, "ignore-stdin", 0, "do not attempt to read stdin")
-	flagSet.StringVarLong(&timeout, "timeout", 0, "Timeout seconds that you allow the whole operation to take")
-	flagSet.Parse(os.Args)
-
-	// Check stdin
-	if !ignoreStdin && !isatty.IsTerminal(os.Stdin.Fd()) {
-		inputOptions.ReadStdin = true
-	}
-
-	// Parse --print
-	if err := parsePrintFlag(printFlag, outputOptions); err != nil {
-		return nil, nil, nil, nil, err
-	}
-
-	// Parse --timeout
-	d, err := parseDurationOrSeconds(timeout)
-	if err != nil {
-		return nil, nil, nil, nil, err
-	}
-	requestOptions.Timeout = d
-
-	return flagSet, inputOptions, requestOptions, outputOptions, nil
-}
-
-func parsePrintFlag(printFlag string, outputOptions *output.Options) error {
-	if printFlag == "\000" {
-		// --print is not specified
-		if isatty.IsTerminal(os.Stdout.Fd()) {
-			outputOptions.PrintResponseHeader = true
-			outputOptions.PrintResponseBody = true
-		} else {
-			outputOptions.PrintResponseBody = true
-		}
-	} else {
-		for _, c := range printFlag {
-			switch c {
-			case 'H':
-				outputOptions.PrintRequestHeader = true
-			case 'B':
-				outputOptions.PrintRequestBody = true
-			case 'h':
-				outputOptions.PrintResponseHeader = true
-			case 'b':
-				outputOptions.PrintResponseBody = true
-			default:
-				return errors.Errorf("Invalid char in --print value (must be consist of HBhb): %c", c)
-			}
-		}
-	}
-	return nil
-}
-
-func parseDurationOrSeconds(timeout string) (time.Duration, error) {
-	if reNumber.MatchString(timeout) {
-		timeout += "s"
-	}
-	d, err := time.ParseDuration(timeout)
-	if err != nil {
-		return time.Duration(0), errors.Errorf("Value of --timeout must be a number or duration string: %v", timeout)
-	}
-	return d, nil
-}
